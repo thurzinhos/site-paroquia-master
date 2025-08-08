7a3d3e0207e358926aecd8373b5a54f16e03fd02
Joaquim Nardino <joaquim.5922@aluno.pr.senac.br>
2025-08-08 12:58:10 -0300

    Update sacramentos.php

--- sacramentos.php
+++ sacramentos.php
@@ -19,7 +19,7 @@ $mysqli->close();
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Paróquia Nossa Senhora de Fátima - Sacramentos</title>
-    <link rel="stylesheet" href="/styles/sacramentos.css" />
+    <link rel="stylesheet" href="./styles/sacramentos.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"/>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
