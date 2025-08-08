2f5c626030c1dd61db9d4fe4bb576faad1bbe8cf
Joaquim Nardino <joaquim.5922@aluno.pr.senac.br>
2025-08-08 12:59:36 -0300

    .

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Paróquia Nossa Senhora de Fátima - Cancelli</title>
-    <link rel="stylesheet" href="/styles/style.css" />
+    <link rel="stylesheet" href="./styles/style.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"/>
     <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
 </head>
